9caac971e6915a4952a6f008ae926e647bced1f5
Paulo Schiavon <schiavon.paulo@gmail.com>
2011-05-26 21:20:29 -0300

    estrutura inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pensino::Application.config.secret_token = '1cfb1196a292d94bea3ec407edb5932c5ea0cea30f3c2025e1c2ea86877e1a7634488453ee09932f33892b663949614bbccfd32abacaa57d38900cbf52513ac3'
