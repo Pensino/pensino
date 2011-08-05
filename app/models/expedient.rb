a42328479dc678e4870d1e0730a5806aa271b6d7
Lucas Angelino <lucas.angelino@gmail.com>
2011-08-04 22:02:45 -0300

    Alteração do tratamento dos dias da semana

--- app/models/expedient.rb
+++ app/models/expedient.rb
@@ -12,48 +12,29 @@
 #
 class Expedient < ActiveRecord::Base
 
-  has_many    :expedient_time_tables
+  WEEKDAYS = ["Domingo",
+              "Segunda-Feira",
+              "Terça-Feira",
+              "Quarta-Feira",
+              "Quinta-Feira",
+              "Sexta-Feira",
+              "Sabado" ]
 
-  validates   :day, :start_time, :end_time, :quantity_lessons, :presence => true
+  has_many :expedient_time_tables
+
+  validates :day, :start_time, :end_time, :quantity_lessons, :presence => true
+  validates :day, :inclusion => WEEKDAYS
 
   validate :end_time_is_after_start_time
 
-  # TODO: arrumar isso
   def name
-    if day == 1
-      name = 'Segunda-Feira ::' + start_time.to_s + ' :: ' + end_time.to_s + ' :: ' + quantity_lessons.to_s
-    end
-
-    if day == 2
-      name = 'Terça-Feira ::' + start_time.to_s + ' :: ' + end_time.to_s + ' :: ' + quantity_lessons.to_s
-    end
-
-    if day == 3
-      name = 'Quarta-Feira ::' + start_time.to_s + ' :: ' + end_time.to_s + ' :: ' + quantity_lessons.to_s
-    end
-
-    if day == 4
-      name = 'Quinta-Feira ::' + start_time.to_s + ' :: ' + end_time.to_s + ' :: ' + quantity_lessons.to_s
-    end
-
-    if day == 5
-      name = 'Sexta-Feira ::' + start_time.to_s + ' :: ' + end_time.to_s + ' :: ' + quantity_lessons.to_s
-    end
-
-    if day == 6
-      name = 'Sábado ::' + start_time.to_s + ' :: ' + end_time.to_s + ' :: ' + quantity_lessons.to_s
-    end
-
-    if day == 7
-      name = 'Domingo ::' + start_time.to_s + ' :: ' + end_time.to_s + ' :: ' + quantity_lessons.to_s
-    end
-
-    name
+    "#{day}::#{start_time.strftime("%H:%M:%S")}::#{end_time.strftime("%H:%M:%S")}::#{quantity_lessons}"
   end
 
   private
   def end_time_is_after_start_time
-    errors.add( 'end_time', 'A hora final deve ser maior que a hora inicial' ) unless start_time < end_time
+    errors.add('end_time', 'A hora final deve ser maior que a hora inicial') unless start_time < end_time
   end
+
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110709205601) do
+ActiveRecord::Schema.define(:version => 20110805001609) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "addressable_id"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(:version => 20110709205601) do
   end
 
   create_table "expedients", :force => true do |t|
-    t.integer  "day"
+    t.string   "day"
     t.time     "start_time"
     t.time     "end_time"
     t.integer  "quantity_lessons"
